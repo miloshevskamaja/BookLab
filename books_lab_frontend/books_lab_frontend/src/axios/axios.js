bb494ef2a1b248da8462386522e720dbd511f453
Maja <miloshevskamaja@yahoo.com>
2025-06-27 16:47:20 +0200

    Completed dockerization and orchestration

--- books_lab_frontend/books_lab_frontend/src/axios/axios.js
+++ books_lab_frontend/books_lab_frontend/src/axios/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const token = "eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6WyJST0xFX0xJQlJBUklBTiJdLCJzdWIiOiJtbSIsImlhdCI6MTc1MDg2NTI5MiwiZXhwIjoxNzUxNzI5MjkyfQ.sOFaV9oHxqMx0tmEWQNBMDugnW-1pNkYNSwkqSLangQ";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:9090",
+    baseURL: "http://localhost:8081",
     headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
